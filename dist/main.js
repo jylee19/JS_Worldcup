b47bb6dfa87d874d792e9596f8bb81895e88eccf
Justin Lee <jylee@Justins-MacBook-Pro.local>
2021-05-11 00:39:30 -0700

    World Cup changes

--- dist/main.js
+++ dist/main.js
@@ -14,13 +14,8 @@
 // };
 document.addEventListener("DOMContentLoaded", function () {
   var app = document.getElementById('root');
-  var request = new XMLHttpRequest();
-  request.open('GET', 'http://footballdb.herokuapp.com/api/event/world.2014/round/20?callback=?', true);
 
-  request.onload = function () {
-    var data = JSON.parse(this.response);
-    console.log(data);
-  }; // let content = document.querySelector(".content");
+// let content = document.querySelector(".content");
   // router = new Router(content, routes);
   // router.start();
   // window.location.hash = "#main";
--- src/index.js
+++ src/index.js
@@ -15,12 +15,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const app = document.getElementById('root');
 
-    var request = new XMLHttpRequest();
-    request.open('GET', 'http://footballdb.herokuapp.com/api/event/world.2014/round/20?callback=?', true);
-    request.onload = function() {
-        var data = JSON.parse(this.response);
-        console.log(data);
-    }
+
 
     // let content = document.querySelector(".content");
     // router = new Router(content, routes);
